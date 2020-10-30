02913bcea0b051a802df40797c1c88cd4adfa746
jyyyyyyyyy <55840975+jyyyyyyyyy@users.noreply.github.com>
2020-10-30 23:12:32 +0800

    Update imaging.py
    Updated no_image + added mount image function

--- venv/imaging.py
+++ venv/imaging.py
@@ -1,21 +1,31 @@
 import os
 import subprocess
 
+# User input image file name
 def with_image():
     disk_input = input("Enter disk file: ")
     return disk_input
 
+# Displays devices and get image
 def no_image():
     # Display images
-    lsblk_command = 'lsblk'
+    lsblk_command = 'lsblk -f /dev/sd*'
     os.system(lsblk_command)
 
     disk_input = input("Enter disk to image: ")
     find_blocksize = 'blockdev --getbsz /dev/' + disk_input
     blocksize = os.popen(find_blocksize).read()
-    print(blocksize)
 
     command = 'dd if=/dev/' + disk_input + ' of=' + disk_input + '.img bs=' + blocksize + ' conv=noerror,sync ' \
                                                                                           'status=progress '
-    print(command)
+    os.system(command)
+    
+# Mount image file
+def mount_image():
+    image_file = input("Creating a mounting point: ")
+    command = "mkdir /mnt/gummybear"
+    os.system(command)
+
+
+    command = "mount -o loop " + image_file + " /mnt/gummybear"
     os.system(command)

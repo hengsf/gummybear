cc6833bab4689494cab851ff1e46154b25ed8c37
jyyyyyyyyy <55840975+jyyyyyyyyy@users.noreply.github.com>
2020-10-31 03:10:59 +0800

    Update imaging.py
    updated + add functions  + working copy

--- venv/imaging.py
+++ venv/imaging.py
@@ -1,31 +1,112 @@
 import os
 import subprocess
 
-# User input image file name
+# User with an image file
 def with_image():
-    disk_input = input("Enter disk file: ")
-    return disk_input
+    disk_input = input("Enter path of disk file: ")
 
-# Displays devices and get image
+    # Get hash of before
+    command = 'sha256 sum ' + disk_input
+    hash_before = os.popen(command).read()
+
+    # Create a working copy of image
+    command = disk_input + '.split("/")[-1]'
+    file_name = os.popen(command).read()
+
+    command = 'cp ' + disk_input + ' ' + file_name + '_backup.img'
+    os.system(command)
+
+    # Get hash of after
+    command = 'sha256 sum ' + file_name + '_backup.img'
+    hash_after = os.popen(command).read()
+
+    if hash_before == hash_after:
+        dir = os.popen('pwd').read()
+        statement = dir + disk_input + '_backup.img'
+        print("A backup image file has been created at: " + statement)
+
+    else:
+        print('Hash of image does not match original, run the program again.')
+        command = ' rm ' + disk_img + '_backup.img'
+        os.system(command)
+
+
+# User without an image file
 def no_image():
-    # Display images
+    # Display devices
     lsblk_command = 'lsblk -f /dev/sd*'
     os.system(lsblk_command)
 
-    disk_input = input("Enter disk to image: ")
-    find_blocksize = 'blockdev --getbsz /dev/' + disk_input
+    disk_input = input("Enter disk to image (sd*): ")
+
+    # Path to disks
+    disk_dev = '/dev/' + disk_input
+    disk_img = disk_input + '.img'
+
+    # Get hash of before
+    command = 'sha256 sum ' + disk_input
+    hash_before = os.popen(command).read()
+
+    find_blocksize = 'blockdev --getbsz ' + disk_dev
     blocksize = os.popen(find_blocksize).read()
 
-    command = 'dd if=/dev/' + disk_input + ' of=' + disk_input + '.img bs=' + blocksize + ' conv=noerror,sync ' \
+    # Creating an image
+    command = 'dd if=' + disk_dev + ' of=' + disk_img + ' bs=' + blocksize + ' conv=noerror,sync ' \
                                                                                           'status=progress '
+
     os.system(command)
-    
+
+    dir = os.popen('pwd').read()
+    statement = 'An image has been create: ' + dir + disk_img
+    print(statement)
+
+    # Get hash of after
+    command = 'sha256 sum ' + disk_img
+    hash_after = os.popen(command).read()
+
+    # Compare hash
+    if hash_before == hash_after:
+        # Create a working copy of image
+        command = 'cp ' + disk_img + disk_input + '_backup.img'
+        os.system(command)
+        statement = dir + disk_input + '_backup.img'
+        print("A backup image file has been created at: " + statement)
+
+    else:
+        print('Hash of image does not match original, run the program again.')
+        command = 'rm ' + dir + disk_img
+        os.system(command)
+        command = 'rm ' + dir + disk_img + '_backup.img'
+        os.system(command)
+
 # Mount image file
 def mount_image():
     image_file = input("Creating a mounting point: ")
-    command = "mkdir /mnt/gummybear"
+    command = 'mkdir /mnt/gummybear'
+    os.system(command)
+
+    command = 'mount -o loop ' + image_file + ' /mnt/gummybear'
     os.system(command)
 
 
-    command = "mount -o loop " + image_file + " /mnt/gummybear"
+# Print directories to choose from
+def find_directories():
+    # Filtering of directories with grep and regular expression
+    # Extend file obmitted with NTFS (not applicable for scanning)
+    # OrphanFile no within project scanning scope
+    regex = ' | grep -oP "(?<=(:\t)).*" | grep -vwE "OrphanFiles" | grep -vwE "Extend"'
+    image_file = input("Img location: ")
+
+    command = 'fls -D -u ' + image_file + regex
     os.system(command)
+
+
+def find_files():
+    image_file = input("Img location: ")
+    regex = ' | grep -i -w "Work" | grep -w "r/r" | grep -oP "(?<=(:\t)).*" '
+    command = 'fls -r -p  ' + image_file + regex
+    os.system(command)
+
+# Uncomment this when testing in this file
+find_directories()
+find_files()

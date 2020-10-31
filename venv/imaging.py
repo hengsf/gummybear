1447402723ccddc1ff5ca7b68f568ac5253c9d6f
jyyyyyyyyy <55840975+jyyyyyyyyy@users.noreply.github.com>
2020-10-31 20:05:28 +0800

    Update imaging.py
    test working copy

--- venv/imaging.py
+++ venv/imaging.py
@@ -4,15 +4,14 @@ import backend
 
 # User with an image file
 def with_image():
-    disk_input = input("Enter path of disk file: ")
+    disk_input = input("Enter full path of disk file: ")
 
     # Get hash of before
     command = 'sha1sum ' + disk_input
     hash_before = os.popen(command).read()
 
     # Create a working copy of image
-    txt = '/home/l/Desktop/ForensicGUI/venv/sdb3.img'
-    file_name = txt.split("/")[-1].split(".")[0]
+    file_name = disk_input.split("/")[-1].split(".")[0]
 
     command = 'cp ' + disk_input + ' ' + file_name + '_backup.img'
     os.system(command)
@@ -21,9 +20,12 @@ def with_image():
     command = 'sha1sum ' + file_name + '_backup.img'
     hash_after = os.popen(command).read()
 
+    # File paths
+    dir = os.popen('pwd').read().rstrip()
+    statement = dir + '/' + file_name + '_backup.img'
+        
     if hash_before[:40] == hash_after[:40]:
-        dir = os.popen('pwd').read()
-        statement = dir + file_name + '_backup.img'
+        
         print("A backup image file has been created at: " + statement)
 
     else:
@@ -51,7 +53,7 @@ def no_image():
     hash_before = os.popen(command).read()
 
     find_blocksize = 'blockdev --getbsz ' + disk_dev
-    blocksize = os.popen(find_blocksize).read()
+    blocksize = os.popen(find_blocksize).read().rstrip()
 
     # Creating an image
     command = 'dd if=' + disk_dev + ' of=' + disk_img + ' bs=' + blocksize + ' conv=noerror,sync ' \
@@ -59,7 +61,7 @@ def no_image():
 
     os.system(command)
 
-    dir = os.popen('pwd').read()
+    dir = os.popen('pwd').read().rstrip()
     statement = 'An image has been create: ' + dir + disk_img
     print(statement)
 
@@ -87,86 +89,3 @@ def no_image():
         exit()
 
     backend.processing(backup_location)
-# Uncomment this when testing in this file
-# with_image()
-# no_image()
-# # User with an image file
-# def with_image():
-#     disk_input = input("Enter path of disk file: ")
-
-#     # Get hash of before
-#     command = 'sha1sum ' + disk_input
-#     hash_before = os.popen(command).read()
-
-#     # Create a working copy of image
-#     txt = '/home/l/Desktop/ForensicGUI/venv/sdb3.img'
-#     file_name = txt.split("/")[-1].split(".")[0]
-
-#     command = 'cp ' + disk_input + ' ' + file_name + '_backup.img'
-#     os.system(command)
-
-#     # Get hash of after
-#     command = 'sha1sum ' + file_end + '_backup.img'
-#     hash_after = os.popen(command).read()
-
-#     if hash_before[:40] == hash_after[:40]:
-#         dir = os.popen('pwd').read()
-#         statement = dir + file_name + '_backup.img'
-#         print("A backup image file has been created at: " + statement)
-
-#     else:
-#         print('Hash of image does not match original, run the program again.')
-#         command = ' rm ' + file_name + '_backup.img'
-#         os.system(command)
-#         exit()
-
-#    # processing(statement)
-
-# # User without an image file
-# def no_image():
-#         # Display devices
-#     lsblk_command = 'lsblk -f /dev/sd*'
-#     os.system(lsblk_command)
-
-#     disk_input = input("Enter disk to image (sd*): ")
-
-#     # Path to disks
-#     disk_dev = '/dev/' + disk_input
-#     disk_img = disk_input + '.img'
-
-#     # Get hash of before
-#     command = 'sha1sum ' + disk_dev
-#     hash_before = os.popen(command).read()
-
-#     find_blocksize = 'blockdev --getbsz ' + disk_dev
-#     blocksize = os.popen(find_blocksize).read()
-
-#     # Creating an image
-#     command = 'dd if=' + disk_dev + ' of=' + disk_img + ' bs=' + blocksize + ' conv=noerror,sync ' \
-#                                                                                           'status=progress '
-
-#     os.system(command)
-
-#     dir = os.popen('pwd').read()
-#     statement = 'An image has been create: ' + dir + disk_img
-#     print(statement)
-
-#     # Get hash of after
-#     command = 'sha1sum ' + disk_img
-#     hash_after = os.popen(command).read()
-
-#     # Compare hash
-#     if hash_before[:40] == hash_after[:40]:
-#         # Create a working copy of image
-#         command = 'cp ' + disk_img + ' ' + disk_input + '_backup.img'
-#         os.system(command)
-#         statement = dir + disk_input + '_backup.img'
-#         print("A backup image file has been created at: " + statement)
-
-#     else:
-#         print('Hash of image does not match original, run the program again.')
-#         command = 'rm ' + dir + disk_img
-#         os.system(command)
-#         command = 'rm ' + dir + disk_input + '_backup.img'
-#         os.system(command)
-#         exit()

188f88733393edcbae0f7d37f5cdba8b6553083c
jyyyyyyyyy <55840975+jyyyyyyyyy@users.noreply.github.com>
2020-10-31 20:05:26 +0800

    Update backend.py
    Test working copy

--- venv/backend.py
+++ venv/backend.py
@@ -2,18 +2,23 @@ import os
 import subprocess
 import VT
 
-# Mount image file
+# Mount image file for processing
 def mount_image(image_file):
     print("Creating a mounting point at /mnt/gummybear")
     command = 'mkdir /mnt/gummybear'
     os.system(command)
 
-    command = 'mount -o loop ' + image_file + ' /mnt/gummybear'
-    os.system(command)
-
-    print("Image has been mounted at /mnt/gummybear. \n")
-
-
+    try:
+        command = 'mount -o loop ' + image_file + ' /mnt/gummybear'
+        os.system(command)
+        print("Image has been mounted at /mnt/gummybear. \n")
+        
+     except:
+        print("WARNING: Mount point in use, unmounting in progress. Exit mount point if required.")
+        command = 'umount /dev/gummybear'
+        os.system(command)
+        mount_image(image_file)
+       
 # Print directories to choose from
 def find_directories(image_file):
     # Filtering of directories with grep and regular expression
@@ -25,22 +30,17 @@ def find_directories(image_file):
     os.system(command)
 
     find_files(image_file)
-
-
+    
+# Print all files in folder (including files in subfolders)
 def find_files(image_file):
     folder = input("Enter folder to view: ")
     regex = ' | grep -i -w "' + folder + '" | grep -w "r/r" | grep -oP "(?<=(:\t)).*" '
     command = 'fls -r -p  ' + image_file + regex
     os.system(command)
-    VT.get_path()
-
-
+    VT.get_path_2()
+    
+# Main event caller
 def processing(image_file):
     mount_image(image_file)
-    find_directories(image_file)
-
-# Uncomment this when testing in this file
-
-# mount_image()
-# find_directories()
-# find_files()
+    while(1):
+        find_directories(image_file)
